93aa0daca4ccc498ee4b5ec20d8ad5eac875dd0a
rotiroti <rotirotirafa@gmail.com>
2021-03-09 22:07:57 -0300

    ajustes no resource e instalando novas lib

--- src/app.py
+++ src/app.py
@@ -1,21 +1,20 @@
 from decouple import Config
 from flask import Flask
 from flask_restful import Api
+from healthcheck import HealthCheck
+
+from resources.users import Users
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://rafael_rotiroti:123456@127.0.0.1:5432/flask"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# from src.models.user import User
-
 api = Api(app)
-#
-api.add_resource(users, '/users')
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+health = HealthCheck(app, '/v1/health')
+api.add_resource(Users, '/users', '/users/<int:user_id>')
+
 
 if __name__ == '__main__':
     app.run(debug=Config('DEBUG'))
--- src/resources/users.py
+++ src/resources/users.py
@@ -1,6 +1,32 @@
+from typing import Dict
+
 from flask_restful import Resource
+from flask import jsonify, request
+
 
 class Users(Resource):
+    http_methods_allowed = ['GET', 'POST', 'PUT', 'DELETE']
 
-    def get(self):
-        return True
+    def get(self) -> Dict:
+        users = [
+            {
+                "user_id": 1,
+                "name": "rafael",
+                "age": 25
+            },
+            {
+                "user_id": 2,
+                "name": "ana",
+                "age": 24
+            }
+        ]
+        return {'users': users}
+
+    def put(self, user_id) -> Dict:
+        return {'Update': user_id}
+
+    def post(self) -> Dict:
+        return jsonify(request.get_json())
+
+    def delete(self, user_id) -> Dict:
+        return {'Deleted': user_id}
